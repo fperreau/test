a2010ce98278e82f7b3be349ffee50bdeff97a74
Frederic Perreau <Frederic.Perreau@kyndryl.com>
2023-08-16 16:44:50 +0200

    test copilot help for date manipulation

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+import datetime
+
 print('Hello FRED')
+
+def CalculateDayBetweenDates(beginDate, endDate):
+    return (endDate - beginDate).days
+
+delay = CalculateDayBetweenDates(datetime.date(2017, 12, 28), datetime.date(2018, 1, 1))
+
+for i in reversed(range(delay)):
+    print(datetime.date(2017, 12, 28) + datetime.timedelta(days=i))
+    
